c49c2800bebad0c4bff1a915c2909dc3b565cca9
AbdulRehman787 <88674059+AbdulRehman787@users.noreply.github.com>
2021-09-13 06:59:21 -0700

    Add files via upload

---
+++ ap.js
@@ -0,0 +1,15 @@
+function signup() {
+    var user_resturant = document.getElementById("resturant").value;
+    var user_name = document.getElementById("name").value;
+    var user_email = document.getElementById("email").value;
+    var user_phone = document.getElementById("phone").value;
+    var user_city = document.getElementById("city").value;
+    var user_pass = document.getElementById("password").value;
+
+    localStorage.setItem("resturant", user_resturant)
+    localStorage.setItem("name", user_name)
+    localStorage.setItem("email", user_email)
+    localStorage.setItem("phone", user_phone)
+    localStorage.setItem("city", user_city)
+    localStorage.setItem("password", user_pass)
+}
